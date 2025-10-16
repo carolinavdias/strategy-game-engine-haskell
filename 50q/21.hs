edb4ad6730b0947324d85bad317e4e4c74c14860
charolzz <a111179@alunos.uminho.pt>
2025-10-16 10:54:11 +0100

    adicionei função para testar

--- 50q/21.hs
+++ 50q/21.hs
@@ -1,7 +1,18 @@
 isPrime :: Int -> Bool
-isPrime x = if x<= 2 then True
-	 else isPrimeAux x 2
+isPrime x = if x == 2 then True
+            else if x < 2 then False
+            else isPrimeAux x 2
 
+isPrimeAux :: Int -> Int -> Bool
 isPrimeAux n m = if m*m > n then True 
-		else if mod n m == 0 then False 
-		else isPrimeAux n (m+1) 
+                 else if mod n m == 0 then False 
+                 else isPrimeAux n (m+1)
+
+teste_isP :: [Bool]
+teste_isP = [
+    isPrime 27 == False,   
+    isPrime 3 == True,     
+    isPrime 5 == True,   
+    isPrime 8 == False,    
+    isPrime 144 == False   
+    ]
