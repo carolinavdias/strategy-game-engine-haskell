030a30638f362fed72df45d11e96ed081285a88f
Leonor1369 <a109820@alunos.uminho.pt>
2025-10-16 10:54:31 +0100

    adicionei função teste

--- 50q/30.hs
+++ 50q/30.hs
@@ -7,3 +7,11 @@ intersect (x:xs) ys
   | x `elem` ys = x : intersect xs ys
   | otherwise   = intersect xs ys
 
+teste :: [Bool]
+teste = [
+	intersect [1,1,2,3,4] [1,3,5] == [1,1,3],
+	intersect [1,3,5,6] [2,4,6] == [6],
+	intersect [4,5,6] [5,5,7] == [5],
+	intersect [] [1,2,3,4] == [],
+	intersect [3,8,9] [] == []
+    ]
