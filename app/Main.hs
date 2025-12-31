d5754eb1b8ab165a2b92046c6c4e8ad80d212807
charolzz <a111179@alunos.uminho.pt>
2025-12-31 03:21:02 +0000

    Integra modo treino, selecao de mapa e carregamento de novos assets

--- app/Main.hs
+++ app/Main.hs
@@ -42,10 +42,9 @@ main = do
   putStrLn "Iniciando Worms..."
   assets <- carregarAssets
   
-  -- Cria contador que começa em 0 e incrementa a cada jogo
   contadorMapas <- newIORef (0 :: Int)
 
-  let estadoInicial = Menu (EstadoMenu OpcaoPlay 0.0 )
+  let estadoInicial = Menu (EstadoMenu OpcaoPlay 0.0)
   
   playIO window background fps estadoInicial
        (\estado -> return (desenhar assets estado))
@@ -75,6 +74,7 @@ carregarAssets = do
   gameBg <- carregarImagem "assets/backgrounds/game_background.png" "Background jogo"
   victoryG <- carregarImagem "assets/backgrounds/victory_green.png" "Vitoria verde"
   victoryB <- carregarImagem "assets/backgrounds/victory_blue.png" "Vitoria azul"
+  mapSelBg <- carregarImagem "assets/backgrounds/map_selection_background.png" "Background selecao mapa"
   
   putStrLn "\n  Sprites minhocas:"
   verdeIdle <- carregarImagem "assets/sprites/minhoca_verde_idle.png" "Verde idle"
@@ -87,6 +87,7 @@ carregarAssets = do
   azulsalta <- carregarImagem "assets/sprites/minhoca_azul_salta.png" "Azul Salta"
   wormGreenBig <- carregarImagem "assets/sprites/worm_green_big.png" "Minhoca verde grande"
   wormBlueBig <- carregarImagem "assets/sprites/worm_blue_big.png" "Minhoca azul grande"
+  wormTrainingBig <- carregarImagem "assets/sprites/worm_training_big.png" "Minhoca treino grande"
   
   putStrLn "  Sprites com armas:"
   greenBazuca <- carregarImagem "assets/sprites/worm_green_bazuca.png" "Verde bazuca"
@@ -100,6 +101,17 @@ carregarAssets = do
   blueEscavadora <- carregarImagem "assets/sprites/worm_blue_escavadora.png" "Azul escavadora"
   blueJetpack <- carregarImagem "assets/sprites/worm_blue_jetpack.png" "Azul jetpack"
 
+  putStrLn "\n  Sprites robô (Bot):"
+  robotBigImg <- carregarImagem "assets/sprites/worm_robot_big.png" "Robo grande"
+  robotIdleImg <- carregarImagemOpcional "assets/sprites/robot_idle.png" "Robo idle"
+  robotWalk1Img <- carregarImagemOpcional "assets/sprites/robot_walk1.png" "Robo walk1"
+  robotWalk2Img <- carregarImagemOpcional "assets/sprites/robot_walk2.png" "Robo walk2"
+  robotBazucaImg <- carregarImagemOpcional "assets/sprites/robot_bazuca.png" "Robo bazuca"
+  robotDinamiteImg <- carregarImagemOpcional "assets/sprites/robot_dinamite.png" "Robo dinamite"
+  robotMinaImg <- carregarImagemOpcional "assets/sprites/robot_mina.png" "Robo mina"
+  robotEscavadoraImg <- carregarImagemOpcional "assets/sprites/robot_escavadora.png" "Robo escavadora"
+  robotJetpackImg <- carregarImagemOpcional "assets/sprites/robot_jetpack.png" "Robo jetpack"
+
   putStrLn "\n  Objetos e armas:"
   barril <- carregarImagem "assets/objetos/barril.png" "Barril"
   bazuca <- carregarImagem "assets/objetos/bazuka.png" "Bazuca"
@@ -111,7 +123,6 @@ carregarAssets = do
   exp2 <- carregarImagem "assets/objetos/explosao2.png" "Explosao 2"
   exp3 <- carregarImagem "assets/objetos/explosao3.png" "Explosao 3"
 
-  -- Terreno 
   putStrLn " Carregar terreno ..."
   pedra <- carregarImagem "assets/terreno/pedra.png" "Pedra"
   agua <- carregarImagem "assets/terreno/agua.png" "Água"
@@ -131,10 +142,20 @@ carregarAssets = do
   timerB <- carregarImagem "assets/ui/timer_blue.png" "Timer azul"
   btnWeapG <- carregarImagem "assets/ui/button_weapons_green.png" "Botao armas verde"
   btnWeapB <- carregarImagem "assets/ui/button_weapons_blue.png" "Botao armas azul"
+  btnWeapR <- carregarImagem "assets/ui/button_weapons_robot.png" "Botao armas robo"
   heart <- carregarImagemOpcional "assets/ui/heart_icon.png" "Icone coracao"
   weapSlot <- carregarImagemOpcional "assets/ui/weapon_slot.png" "Slot arma"
   btnRestart <- carregarImagem "assets/ui/button_restart.png" "Botao restart"
   btnMenu <- carregarImagem "assets/ui/button_menu.png" "Botao menu"
+  textTreinoImg <- carregarImagem "assets/ui/text_treino.png" "Texto Treino"
+  
+  putStrLn "\n  Seleção de Mapa:"
+  textEscolheMapaImg <- carregarImagem "assets/ui/text_escolhe_mapa.png" "Texto Escolhe Mapa"
+  mapIconClassicoImg <- carregarImagem "assets/ui/map_icon_classico.png" "Icone Classico"
+  mapIconMontanhasImg <- carregarImagem "assets/ui/map_icon_montanhas.png" "Icone Montanhas"
+  mapIconLagoImg <- carregarImagem "assets/ui/map_icon_lago.png" "Icone Lago"
+  mapIconPedreiraImg <- carregarImagem "assets/ui/map_icon_pedreira.png" "Icone Pedreira"
+  mapIconIlhasImg <- carregarImagem "assets/ui/map_icon_ilhas.png" "Icone Ilhas"
   
   putStrLn "\n  Molduras:"
   stoneF <- carregarImagem "assets/frames/stone_frame.png" "Moldura de pedra"
@@ -145,20 +166,26 @@ carregarAssets = do
     { menuAssets = MenuAssets menuBg menuLg btnPlay btnTut btnExit
                               modeBg modeTitle mode2P modeBot modeTraining
                               modeInstr btnBack
-    , backgroundAssets = BackgroundAssets gameBg victoryG victoryB
+    , backgroundAssets = BackgroundAssets gameBg victoryG victoryB mapSelBg
     , spriteAssets = SpriteAssets verdeIdle verdeWalk1 verdeWalk2 verdesalta
                                   azulIdle azulWalk1 azulWalk2 azulsalta
-                                  wormGreenBig wormBlueBig 
+                                  wormGreenBig wormBlueBig wormTrainingBig
                                   greenBazuca greenDinamite greenMina greenEscavadora greenJetpack
                                   blueBazuca blueDinamite blueMina blueEscavadora blueJetpack
+                                  robotBigImg robotIdleImg robotWalk1Img robotWalk2Img
+                                  robotBazucaImg robotDinamiteImg robotMinaImg 
+                                  robotEscavadoraImg robotJetpackImg
     , objetoAssets = ObjetoAssets barril bazuca dinamite mina escavadora
                                   jetpack exp1 exp2 exp3
     , terrenoAsserts = TerrenoAsserts pedra agua terra
     , uiAssets = UIAssets hp0 hp20 hp40 hp60 hp80 hp100
                           textP1 textP2 textCtrl
-                          timerG timerB btnWeapG btnWeapB
-                          heart weapSlot btnRestart btnMenu 
+                          timerG timerB btnWeapG btnWeapB btnWeapR
+                          heart weapSlot btnRestart btnMenu textTreinoImg
     , frameAssets = FrameAssets stoneF
+    , mapSelectionAssets = MapSelectionAssets textEscolheMapaImg 
+                                              mapIconClassicoImg mapIconMontanhasImg
+                                              mapIconLagoImg mapIconPedreiraImg mapIconIlhasImg
     }
 
 -- Carrega uma imagem PNG do disco
@@ -193,6 +220,7 @@ desenhar :: Assets -> EstadoJogo -> Picture
 desenhar assets estado = case estado of
   Menu estadoMenu -> desenharMenu assets estadoMenu
   SelecaoModo estadoSel -> desenharSelecao assets estadoSel
+  SelecaoMapaTreino estadoSel -> desenharSelecaoMapa assets estadoSel  -- NOVO!
   Jogando partida -> Desenhar.desenha assets partida
   GameOver estadoFinal -> desenharGameOver assets estadoFinal
   Victory estadoFinal -> desenharVictory assets estadoFinal
@@ -204,6 +232,8 @@ eventoComContador assets contadorRef ev estado = case estado of
   Menu estadoMenu -> return $ eventoMenu ev estadoMenu
   
   SelecaoModo estadoSel -> eventoSelecaoContador contadorRef ev estadoSel
+  
+  SelecaoMapaTreino estadoSel -> return $ eventoSelecaoMapa ev estadoSel  -- NOVO!
   
   Jogando partida -> return $ eventoJogo ev partida
   
@@ -231,33 +261,86 @@ eventoSelecaoContador contadorRef evento estado = case evento of
   EventKey (SpecialKey KeyTab) Down _ _ ->
     return $ SelecaoModo (estado { modoSelecionado = proximoModo (modoSelecionado estado) })
   
-  -- QUANDO PRESSIONA ENTER: Incrementa contador e escolhe próximo mapa
   EventKey (SpecialKey KeyEnter) Down _ _ -> do
-    contador <- readIORef contadorRef
-    let novoContador = contador + 1
-    writeIORef contadorRef novoContador
-    
-    let mapaEscolhido = selecionarMapa novoContador
-    putStrLn $ "Jogo " ++ show novoContador ++ " - Mapa: " ++ mapaNome mapaEscolhido
-    
-    return $ iniciarComMapa (modoSelecionado estado) mapaEscolhido
+    case modoSelecionado estado of
+      Treino -> return $ SelecaoMapaTreino (EstadoSelecaoMapa 0 0.0)  -- NOVO: Vai para seleção de mapa!
+      Voltar -> return $ Menu (EstadoMenu OpcaoPlay 0.0)
+      modo -> do
+        contador <- readIORef contadorRef
+        let novoContador = contador + 1
+        writeIORef contadorRef novoContador
+        
+        let mapaEscolhido = selecionarMapa novoContador
+        putStrLn $ "Jogo " ++ show novoContador ++ " - Mapa: " ++ mapaNome mapaEscolhido
+        
+        return $ iniciarComMapa modo mapaEscolhido novoContador
   
   EventKey (SpecialKey KeyEsc) Down _ _ ->
     return $ Menu (EstadoMenu OpcaoPlay 0.0)
   
   _ -> return $ SelecaoModo estado
 
+-- EVENTOS NA SELEÇÃO DE MAPA (TREINO)
+-- mapaSelecionado: 0-4 = mapas, 5 = voltar
+eventoSelecaoMapa :: Event -> EstadoSelecaoMapa -> EstadoJogo
+eventoSelecaoMapa evento estado = case evento of
+  -- Navegação com setas esquerda/direita (só entre mapas)
+  EventKey (SpecialKey KeyRight) Down _ _ ->
+    if mapaSelecionado estado == 5
+    then SelecaoMapaTreino estado  -- No voltar, não faz nada
+    else SelecaoMapaTreino (estado { mapaSelecionado = (mapaSelecionado estado + 1) `mod` 5 })
+  
+  EventKey (SpecialKey KeyLeft) Down _ _ ->
+    if mapaSelecionado estado == 5
+    then SelecaoMapaTreino estado  -- No voltar, não faz nada
+    else SelecaoMapaTreino (estado { mapaSelecionado = (mapaSelecionado estado - 1 + 5) `mod` 5 })
+  
+  -- Seta para CIMA: vai para o botão voltar
+  EventKey (SpecialKey KeyUp) Down _ _ ->
+    SelecaoMapaTreino (estado { mapaSelecionado = 5 })
+  
+  -- Seta para BAIXO: vai para os mapas (meio = 2)
+  EventKey (SpecialKey KeyDown) Down _ _ ->
+    if mapaSelecionado estado == 5
+    then SelecaoMapaTreino (estado { mapaSelecionado = 2 })  -- Vai para o mapa do meio
+    else SelecaoMapaTreino estado  -- Já está nos mapas, não faz nada
+  
+  -- Enter confirma seleção
+  EventKey (SpecialKey KeyEnter) Down _ _ ->
+    if mapaSelecionado estado == 5
+    then SelecaoModo (EstadoSelecao Treino 0.0)  -- Voltar!
+    else iniciarTreinoComMapa (mapaSelecionado estado)
+  
+  -- Escape ou X volta para seleção de modo
+  EventKey (SpecialKey KeyEsc) Down _ _ ->
+    SelecaoModo (EstadoSelecao Treino 0.0)
+  
+  EventKey (Char 'x') Down _ _ ->
+    SelecaoModo (EstadoSelecao Treino 0.0)
+  
+  EventKey (Char 'X') Down _ _ ->
+    SelecaoModo (EstadoSelecao Treino 0.0)
+  
+  _ -> SelecaoMapaTreino estado
+
+-- Inicia treino com mapa específico
+iniciarTreinoComMapa :: Int -> EstadoJogo
+iniciarTreinoComMapa idx = 
+  let mapa = selecionarMapa idx
+  in Jogando (criarPartidaComMapa Treino (criarEstadoTreino mapa) idx)
+
 -- EVENTOS DE VITÓRIA COM CONTADOR
 eventoVictoryContador :: IORef Int -> Event -> EstadoFinal -> IO EstadoJogo
-eventoVictoryContador contadorRef (EventKey (Char 'r') Down _ _) _ = do
+eventoVictoryContador contadorRef (EventKey (Char 'r') Down _ _) estado = do
   contador <- readIORef contadorRef
   let novoContador = contador + 1
   writeIORef contadorRef novoContador
   
   let mapaEscolhido = selecionarMapa novoContador
-  putStrLn $ "Reinício " ++ show novoContador ++ " - Mapa: " ++ mapaNome mapaEscolhido
+      modo = modoJogoFinal estado
+  putStrLn $ "Reinício " ++ show novoContador ++ " - Mapa: " ++ mapaNome mapaEscolhido ++ " - Modo: " ++ show modo
   
-  return $ Jogando (criarPartida DoisJogadores (criarEstadoDoMapa mapaEscolhido))
+  return $ iniciarComMapa modo mapaEscolhido novoContador
 
 eventoVictoryContador contadorRef (EventKey (Char 'R') Down m p) estado = 
   eventoVictoryContador contadorRef (EventKey (Char 'r') Down m p) estado
@@ -282,18 +365,20 @@ eventoVictoryContador contadorRef (EventKey (SpecialKey KeyEnter) Down _ _) esta
       writeIORef contadorRef novoContador
       
       let mapaEscolhido = selecionarMapa novoContador
-      putStrLn $ "Restart " ++ show novoContador ++ " - Mapa: " ++ mapaNome mapaEscolhido
+          modo = modoJogoFinal estado
+      putStrLn $ "Restart " ++ show novoContador ++ " - Mapa: " ++ mapaNome mapaEscolhido ++ " - Modo: " ++ show modo
       
-      return $ Jogando (criarPartida DoisJogadores (criarEstadoDoMapa mapaEscolhido))
+      return $ iniciarComMapa modo mapaEscolhido novoContador
     
     VoltarMenu -> return $ Menu (EstadoMenu OpcaoPlay 0.0)
 
 eventoVictoryContador _ _ estado = return $ Victory estado
 
 -- FUNÇÃO AUXILIAR: Inicia jogo com mapa específico
-iniciarComMapa :: ModoJogo -> MapaCompleto -> EstadoJogo
-iniciarComMapa Voltar _ = Menu (EstadoMenu OpcaoPlay 0.0)
-iniciarComMapa modo mapa = Jogando (criarPartida modo (criarEstadoDoMapa mapa))
+iniciarComMapa :: ModoJogo -> MapaCompleto -> Int -> EstadoJogo
+iniciarComMapa Voltar _ _ = Menu (EstadoMenu OpcaoPlay 0.0)
+iniciarComMapa Treino mapa idx = Jogando (criarPartidaComMapa Treino (criarEstadoTreino mapa) idx)
+iniciarComMapa modo mapa idx = Jogando (criarPartidaComMapa modo (criarEstadoDoMapa mapa) idx)
 
 
 -- Atualiza o estado do jogo ao longo do tempo
@@ -301,5 +386,6 @@ atualizar :: Assets -> Float -> EstadoJogo -> EstadoJogo
 atualizar assets dt estado = case estado of
   Menu estadoMenu -> atualizarMenu dt estadoMenu
   SelecaoModo estadoSel -> atualizarSelecao dt estadoSel
+  SelecaoMapaTreino estadoSel -> SelecaoMapaTreino (estadoSel { animacaoSelecaoMapa = animacaoSelecaoMapa estadoSel + dt })
   Jogando partida -> atualizarPartidaCompleta dt partida
   _ -> estado
